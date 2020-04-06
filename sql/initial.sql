65905c81a3a25c1c2b63a0f63248aecb8178345d
mbihrle <bihrle.marco@hotmail.com>
2020-04-06 22:17:10 +0200

    adding database connection

--- sql/initial.sql
+++ sql/initial.sql
@@ -1,6 +1,13 @@
-CREATE TABLE login
-(
+CREATE TABLE login (
     id serial PRIMARY KEY,
     hash varchar(100) NOT NULL,
+    email text UNIQUE NOT NULL
+);
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(100),
     email text UNIQUE NOT NULL,
-)
+    entries BIGINT DEFAULT 0,
+    joined TIMESTAMP NOT NULL
+);
